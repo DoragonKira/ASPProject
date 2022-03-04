157f7199bbca2cf33b7ecda25a8c957628151afb
solracnaj <99858429+solracnaj@users.noreply.github.com>
2022-03-04 10:06:19 +0800

    Add files via upload

--- lab10.py
+++ lab10.py
@@ -1,8 +1,30 @@
 import pandas as pd
-cars = pd.read_csv('mtcars.csv')
 import matplotlib.pyplot as plt
-plt.scatter(cars['mpg'], cars['hp'])
-plt.show();
+
+# reading the file
+excel_file = 'Project_File.xlsx'
+df = pd.read_excel(excel_file)
+
+
+# retrieving the data and finding the sum
+total = df.iloc[:, 1:6].sum()
+
+
+# sorting the values in ascending order
+sorttop3 = total.sort_values()
+
+
+# selecting the top 3 values
+top3 = sorttop3.tail(3)
+print(top3)
+
+# plotting the graph
+plt.xlabel('Countries', fontsize=5)
+plt.ylabel('Total visitors in 10 years', fontsize=10)
+plt.title('Top 3 countries with the most visitors')
+plt.ticklabel_format(style='plain')
+plt.bar(top3.index, top3.values)
+plt.show()
 
 
 
